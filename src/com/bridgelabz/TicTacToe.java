e5ab30fb115b430714832b20cdde63d4b75dbfae
Krunali <krunalilole29@gmail.com>
2022-03-15 20:50:55 +0530

    Added UC3 to Show Board

--- src/com/bridgelabz/TicTacToe.java
+++ src/com/bridgelabz/TicTacToe.java
@@ -3,6 +3,13 @@ package com.bridgelabz;
 import java.util.Scanner;
 public class TicTacToe
 {
+    public static void main(String[] args) {
+
+        TicTacToe obj = new TicTacToe();
+        obj.initialize();
+        obj.showboard();
+
+    }
     int i;
     char[] board = new char[10];
     char player, computer;
@@ -14,14 +21,13 @@ public class TicTacToe
         }
     }
 
-    public void main() {
+    public void showboard() {
         System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
         System.out.println("---------");
         System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
         System.out.println("---------");
         System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
     }
-
     public void check()
     {
         System.out.println("Please Select Your Choice Letter : 'X' or 'O' ");
